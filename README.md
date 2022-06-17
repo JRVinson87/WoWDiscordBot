0cf1bbd2f6eef26bddb2831a6bb65921b05f93ed
Joshua Vinson <JRVinson87@gmail.com>
2022-06-17 17:45:32 -0600

    test init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# WoWDiscordBot
-Learning Discord Bot Python Programming
+Learning Discord Bot Python Programming    
+$commands displays commands    
+$mp charName serverName shows M+ stats for character on specified realm.    
+$arena charName serverName shows Arena stats for character on specified realm.    
+$affix shows current affixes for the week in US realms.    
---
+++ wow_realms.py
@@ -0,0 +1,9 @@
+#dict for realm names that have spaces
+wowRealms = {
+    "area52": "area-52", "bleedinghollow": "bleeding-hollow", "burningblade": "burning-blade", 
+    "emeralddream": "emerald-dream", "moonguard": "moon-guard", "wyrmrestaccord": "wyrmrest-accord", 
+    "burninglegion": "burning-legion", "khazmodan": "khaz-modan", "aeriepeak": "aerie-peak", 
+    "alteracmountains": "alterac-mountains", "shatteredhand": "shattered-hand", 
+    "shadowcouncil": "shadow-council", "earthenring": "earthen-ring", "silverhand": "silver-hand", 
+    "argentdawn": "argent-dawn", "kirintor": "kirin-tor"
+    }
